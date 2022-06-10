c58080bd838069f22082c1d900e76985b3ead5ef
sukhunoia <sukhchain.unoia@gmail.com>
2022-06-10 09:47:33 +0530

    push th chsanges

--- components/BarRange.js
+++ components/BarRange.js
@@ -5,21 +5,21 @@ import styles from '../styles/Home.module.css'
 class BarRange extends React.Component {
   
   valuesmonthly = [
-     { thread:'100',  title:'Unmeterd Residential Elite 100',  pricethread:'$1.99', price:'$199.00' },
-     { thread:'200',  title:'Unmeterd Residential Guru 200', pricethread:'$1.50', price:'$299.00' },
-     { thread:'500',  title:'Unmeterd Residential Mega 500', pricethread:'$1.20', price:'$599.00' },
-     { thread:'1000', title:'Unmeterd Residential Pro 1000', pricethread:'$1.10', price:'$1,099.00' },
-     { thread:'2000', title:'Unmeterd Residential Giga 2000', pricethread:'$1.00', price:'$1,999.00' },
-     { thread:'5000', title:'Unmeterd Residential Gold 5000', pricethread:'$0.88', price:'$4,399.00' },
+     { thread:'100',  title:'Unmetered  Residential Elite 100',  pricethread:'$1.99', price:'$199.00' },
+     { thread:'200',  title:'Unmetered  Residential Guru 200', pricethread:'$1.50', price:'$299.00' },
+     { thread:'500',  title:'Unmetered  Residential Mega 500', pricethread:'$1.20', price:'$599.00' },
+     { thread:'1000', title:'Unmetered  Residential Pro 1000', pricethread:'$1.10', price:'$1,099.00' },
+     { thread:'2000', title:'Unmetered  Residential Giga 2000', pricethread:'$1.00', price:'$1,999.00' },
+     { thread:'5000', title:'Unmetered  Residential Gold 5000', pricethread:'$0.88', price:'$4,399.00' },
     ];
 
     valuesyearly = [
-      { thread:'100',  title:'Unmeterd Residential Elite 100',  pricethread:'$1.67', price: '$1,999.00' },
-      { thread:'200',  title:'Unmeterd Residential Guru 200',  pricethread:'$1.25', price:'$2,999.00' },
-      { thread:'500',  title:'Unmeterd Residential Mega 500',  pricethread:'$1.00', price:'$5,999.00' },
-      { thread:'1000', title:'Unmeterd Residential Pro 1000', pricethread:'$0.92', price:'$10,999.00' },
-      { thread:'2000', title:'Unmeterd Residential Giga 2000', pricethread:'$0.83', price:'$19,999.00' },
-      { thread:'5000', title:'Unmeterd Residential Gold 5000', pricethread:'$0.73', price:'$43,999.00' },
+      { thread:'100',  title:'Unmetered  Residential Elite 100',  pricethread:'$1.67', price: '$1,999.00' },
+      { thread:'200',  title:'Unmetered  Residential Guru 200',  pricethread:'$1.25', price:'$2,999.00' },
+      { thread:'500',  title:'Unmetered  Residential Mega 500',  pricethread:'$1.00', price:'$5,999.00' },
+      { thread:'1000', title:'Unmetered  Residential Pro 1000', pricethread:'$0.92', price:'$10,999.00' },
+      { thread:'2000', title:'Unmetered  Residential Giga 2000', pricethread:'$0.83', price:'$19,999.00' },
+      { thread:'5000', title:'Unmetered  Residential Gold 5000', pricethread:'$0.73', price:'$43,999.00' },
      ];
 
   constructor(props) {
@@ -33,6 +33,17 @@ class BarRange extends React.Component {
 
   handleInputChange = e => {
     this.setState({ currentStepIndex: e.currentTarget.value });
+    let current_thread = this.state.values[e.currentTarget.value].thread;
+    let arry_len = this.state.values.length;
+    let last_thread = this.state.values[arry_len-1].thread;
+    let first_thread = this.state.values[0].thread;
+    if (current_thread == last_thread || current_thread == first_thread){
+      this.setState({ price_amt: '' });
+    }
+    else{
+      this.setState({ price_amt: current_thread});
+    }
+    
   };
 
   handleChange = e => {
@@ -58,7 +69,7 @@ class BarRange extends React.Component {
                   <div className='labels'>
                      <h6><small>●</small> <b>Number Of Threads</b>
                      <span>{this.state.values[this.state.currentStepIndex].thread}
-                       Threads</span>
+                       </span>
                        </h6>
                        <h6><small>●</small> <b> Price per Threads </b>
                          <span>{this.state.values[this.state.currentStepIndex].pricethread}</span>
@@ -67,7 +78,7 @@ class BarRange extends React.Component {
                   <div className='bar-ranges'>     
                   <div className="threads">
                       <span>100 Threads </span>
-                      <span className="ms-4">1000 Threads </span>
+                      <span className={"d-none active" + this.state.price_amt}>{this.state.price_amt} Threads</span>
                       <span>5000 Threads </span>
                     </div>
                     <input
